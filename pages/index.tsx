624a607257ba1432bed6c059759e582c6ec9a4b2
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2022-10-17 21:08:22 -0300

    feat: teste commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,30 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  const [repositories, setRepositories] = useState<string[]>([])
+  const [repositories, setRepositories] = useState<string[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/users/ericksonsiqueira/repos')
-    .then( res => res.json())
-    .then(data => {
-      const repNames = data.map(rep => rep.name);
+      .then((res) => res.json())
+      .then((data) => {
+        const repNames = data.map((rep) => rep.name);
 
-      setRepositories(repNames)
-    })
-  }, [])
+        setRepositories(repNames);
+      });
+  }, []);
 
   return (
     <div>
-      <h1>
-        Hello world!
-      </h1>
+      <h1>Hello world!</h1>
 
       <ul>
         {repositories.map((rep) => (
-          <li key={rep}>
-            {rep}
-          </li>
+          <li key={rep}>{rep}</li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
