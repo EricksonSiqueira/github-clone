59a70ad735945bf9af2b949e544552b167598e23
Erickson Siqueira <ericksonsiqueirasilva@gmail.com>
2022-10-18 19:01:48 -0300

    feat: create interface and get repo name and link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,44 @@
 import React, { useEffect, useState } from 'react';
+import humps from 'humps';
+
+interface RepositoryInfo {
+  svnUrl: string;
+  name: string;
+}
 
 export default function Home(): JSX.Element {
-  const [repositories, setRepositories] = useState<string[]>([]);
+  const [repositories, setRepositories] = useState<RepositoryInfo[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/users/ericksonsiqueira/repos')
       .then(async (res) => await res.json())
       .then((data) => {
-        const repNames = data.map((rep) => rep.name);
+        const repNames = data.map((rep) =>
+          humps.camelizeKeys(rep)
+        ) as RepositoryInfo[];
 
         setRepositories(repNames);
       })
       .catch((err) => console.log(err));
   }, []);
 
+  console.log(repositories);
+
   return (
     <div>
       <h1>Hello world!</h1>
-
       <ul>
         {repositories.map((rep) => (
-          <li key={rep}>{rep}</li>
+          <li key={rep.name}>
+            <a
+              href={rep.svnUrl}
+              title={`link para o repositório${rep.name}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {rep.name}
+            </a>
+          </li>
         ))}
       </ul>
     </div>
